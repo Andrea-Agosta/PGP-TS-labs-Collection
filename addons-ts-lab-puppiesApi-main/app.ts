555e31a27536c24e6c3a57c8de0f518bdd70603c
Andrea-Agosta <andrea.agosta85@gmail.com>
2023-01-03 14:57:13 +0100

    start second lab-puppies

---
+++ addons-ts-lab-puppiesApi-main/app.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import { Request, Response, Application } from 'express';
+
+const app: Application = express();
+
+app.get('/api/test', (_req: Request, res: Response) => {
+  return res.status(200).json({ test: 'is working as it should' });
+});
+
+app.get('api/puppies', (_req: Request, res: Response) => {
+  return res.status(200).json({ puppies: 'hey' });
+});
+
+export default app;

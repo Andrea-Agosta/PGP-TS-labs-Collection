a26a14ffcb30a52e0beb2369f321a56b2a7ed04a
Andrea-Agosta <andrea.agosta85@gmail.com>
2023-01-04 12:09:40 +0100

    add get route to api/puppies

--- addons-ts-lab-puppiesApi-main/app.ts
+++ addons-ts-lab-puppiesApi-main/app.ts
@@ -1,15 +1,56 @@
-// import { db } from 'db';
 import express from 'express';
 import { Request, Response, Application } from 'express';
 
 const app: Application = express();
+app.use(express.json());
+interface PuppiesData {
+  id: number,
+  breed: string,
+  name: string,
+  birth: string
+}
+
+const db: PuppiesData[] = [
+  {
+    "id": 1,
+    "breed": "American Bully",
+    "name": "cfetherstone0",
+    "birth": "11/12/2022"
+  },
+  {
+    "id": 2,
+    "breed": "Australian Bulldog",
+    "name": "fbeaston1",
+    "birth": "3/12/2022"
+  },
+  {
+    "id": 3,
+    "breed": "Biewer Terrier",
+    "name": "mfutcher2",
+    "birth": "8/12/2022"
+  },
+  {
+    "id": 4,
+    "breed": "Beagle",
+    "name": "noram3",
+    "birth": "22/12/2022"
+  },
+];
+
+const getAllPuppies = (_req: Request, res: Response) => {
+  res.status(200).json(db);
+};
+
+const getPuppiesfromId = (_req: Request, res: Response) => {
+  const id: number = Number(_req.params.id);
+  (id && id <= db.length) ? res.status(200).json(db[id - 1]) : res.status(400).json({ error: 'Bad Request' });
+};
 
 app.get('/api/test', (_req: Request, res: Response) => {
   return res.status(200).json({ test: 'is working as it should' });
 });
 
-app.get('api/puppies', (_req: Request, res: Response) => {
-  return res.status(200).json();
-});
+app.get('/api/puppies', getAllPuppies);
+app.get('/api/puppies/:id', getPuppiesfromId);
 
 export default app;
--- addons-ts-lab-puppiesApi-main/test.ts
+++ addons-ts-lab-puppiesApi-main/test.ts
@@ -9,4 +9,20 @@ describe('Testing api endpoint', () => {
       test: 'is working as it should',
     });
   });
+
+  test('GET request to api/puppies', async () => {
+    const res = await request(app).get('api/puppies');
+    expect(res.statusCode).toEqual(200);
+  });
+
+  test('GET puppy with id 1', async () => {
+    const res = await request(app).get('api/puppies/1');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual({
+      "id": 1,
+      "breed": "American Bully",
+      "name": "cfetherstone0",
+      "birth": "11/12/2022"
+    });
+  });
 });
